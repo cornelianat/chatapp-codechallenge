292f7f8b86fd9a0adf0aa6298e9a476a56092de0
ct <cornelianatasha528@gmail.com>
2020-10-23 17:21:28 +0700

    Fix

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -2,6 +2,7 @@ class RoomChannel < ApplicationCable::Channel
   def subscribed
     room = Room.find params[:room]
     stream_for room
+    reject unless current_user.can_access?(@room)
     # stream_from "room_#{params[:room]}"
   end
   def unsubscribed
