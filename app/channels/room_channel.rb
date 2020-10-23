b2d5a2be70f66c180113b94e09233ebe9b0169c1
ct <cornelianatasha528@gmail.com>
2020-10-23 17:09:55 +0700

    Fix

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -4,4 +4,8 @@ class RoomChannel < ApplicationCable::Channel
     stream_for room
     # stream_from "room_#{params[:room]}"
   end
+  def unsubscribed
+    room = Room.find params[:room]
+    redsis.lrem(key_for(page.id),1,current_user.id) 
+  end
 end
